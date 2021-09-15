7798f9c40580a3439293aa97aa8adb909c9e6741
Rayat Al Islam <86007921+rayatalislam@users.noreply.github.com>
2021-09-15 21:45:08 +0600

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,12 @@
+import math
+hour = int(input())
+min = int(input())
+
+res = (hour*60)-(min*11)
+res2 = res/2
+
+if hour < 6:
+	result = res2
+elif hour > 6:
+	result = 360-res2
+print(abs(result))
